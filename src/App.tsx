ff7cb5f69d8c7bd6bd1a1b728bcc226925ce2a02
Otavio Araujo <otavio.netoca@gmail.com>
2022-10-06 13:07:55 -0300

    fix: Fixes linting and formatting errors after adding eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,108 @@
-import React, { FormEvent, useEffect, useMemo, useState } from "react";
-import { Autocomplete } from "./components/Autocomplete/Autocomplete";
+import React, { useEffect, useState } from "react";
+import Autocomplete from "./components/Autocomplete/Autocomplete";
 import useDebounce from "./hooks/useDebounce";
 
 interface CountryName {
-  common: string;
-  official: string;
+    common: string;
+    official: string;
 }
 
 interface CountryFlags {
-  png: string;
-  svg: string;
+    png: string;
+    svg: string;
 }
 
 interface Country {
-  name: CountryName;
-  flags: CountryFlags;
+    name: CountryName;
+    flags: CountryFlags;
 }
 
-function App() {
-  const [value, setValue] = useState<string>("");
-  const [selectedSuggestion, setSelectedSuggestion] = useState<string>("");
-  const [suggestions, setSuggestions] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [errorMessage, setErrorMessage] = useState<string>("");
-  const debounce = useDebounce();
+function App(): JSX.Element {
+    const [value, setValue] = useState<string>("");
+    const [selectedSuggestion, setSelectedSuggestion] = useState<string>("");
+    const [suggestions, setSuggestions] = useState<string[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    const debounce = useDebounce();
 
-  useEffect(() => {
-    if (value.length === 0) {
-      setSuggestions([]);
-      return;
-    }
+    useEffect(() => {
+        if (value.length === 0) {
+            setSuggestions([]);
+            return;
+        }
 
-    if (!!selectedSuggestion) return;
+        if (selectedSuggestion.length > 0) return;
 
-    const fetchCountries = () => {
-      // TODO: Remove the hardcoded url below
-      fetch(`https://restcountries.com/v3.1/name/${value}`)
-        .then(async (response) => {
-          const isJson = response.headers
-            .get("Content-Type")
-            ?.includes("application/json");
-          const data = isJson ? await response.json() : null;
+        const fetchCountries = (): void => {
+            // TODO: Remove the hardcoded url below
+            fetch(`https://restcountries.com/v3.1/name/${value}`)
+                .then(async (response) => {
+                    const isJson =
+                        response.headers
+                            .get("Content-Type")
+                            ?.includes("application/json") ?? false;
+                    const data: Country[] | null = isJson
+                        ? await response.json()
+                        : null;
 
-          if (!response.ok) {
-            // TODO: Improve error handling to deal with specific statuses, e.g.: (401, 405, 500)
-            if (response.status !== 404) {
-              throw new Error(`${response.status} - ${response.statusText}`);
-            }
-          }
+                    if (!response.ok) {
+                        // TODO: Improve error handling to deal with specific statuses, e.g.: (401, 405, 500)
+                        if (response.status !== 404) {
+                            throw new Error(
+                                `${response.status} - ${response.statusText}`
+                            );
+                        }
+                    }
 
-          if (data.length) {
-            setSuggestions(() => [
-              ...(data as Country[]).map((c) => c.name.common),
-            ]);
-            setErrorMessage("");
-          }
-        })
-        .catch((e: Error) => {
-          setErrorMessage(e.message);
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
+                    if (Array.isArray(data)) {
+                        setSuggestions(() => [
+                            ...data.map((c) => c.name.common),
+                        ]);
+                        setErrorMessage("");
+                    }
+                })
+                .catch((e: Error) => {
+                    setErrorMessage(e.message);
+                })
+                .finally(() => {
+                    setIsLoading(false);
+                });
+        };
+
+        setIsLoading(true);
+        debounce(() => {
+            fetchCountries();
+        }, 500);
+    }, [value, selectedSuggestion]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setSelectedSuggestion("");
+        setValue(e.target.value);
     };
 
-    setIsLoading(true);
-    debounce(() => {
-      fetchCountries();
-    }, 500);
-  }, [value, selectedSuggestion]);
+    const handleSelectSuggestion = (suggestion: string): void => {
+        setSelectedSuggestion(suggestion);
+        setValue(suggestion);
+    };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedSuggestion("");
-    setValue(e.target.value);
-  };
-
-  const handleSelectSuggestion = (suggestion: string) => {
-    setSelectedSuggestion(suggestion);
-    setValue(suggestion);
-  };
-
-  return (
-    <div>
-      <Autocomplete
-        id="search-country"
-        label="Country"
-        name="country"
-        placeholder="Type in to look up for a country"
-        isLoading={isLoading}
-        suggestions={suggestions}
-        value={value}
-        onChange={handleChange}
-        onSelectSuggestion={handleSelectSuggestion}
-      />
-      {(!!errorMessage && (
-        <span style={{ color: "darkred" }}>{errorMessage}</span>
-      )) ||
-        null}
-    </div>
-  );
+    return (
+        <div>
+            <Autocomplete
+                id="search-country"
+                label="Country"
+                name="country"
+                placeholder="Type in to look up for a country"
+                isLoading={isLoading}
+                suggestions={suggestions}
+                value={value}
+                onChange={handleChange}
+                onSelectSuggestion={handleSelectSuggestion}
+            />
+            {errorMessage.length > 0 ? (
+                <span style={{ color: "darkred" }}>{errorMessage}</span>
+            ) : null}
+        </div>
+    );
 }
 
 export default App;
--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -2,20 +2,18 @@ import { useRef } from "react";
 
 const MIN_TIMEOUT_IN_MILLISECONDS = 200;
 
-const useDebounce = () => {
-  const timeoutIdRef = useRef<number>(0);
+const useDebounce = (): Function => {
+    const timeoutIdRef = useRef<number>(0);
 
-  return (func: Function, ms = 1000) => {
-    const timeout =
-      ms < MIN_TIMEOUT_IN_MILLISECONDS ? MIN_TIMEOUT_IN_MILLISECONDS : ms;
+    return (func: Function, ms = 1000) => {
+        const timeout =
+            ms < MIN_TIMEOUT_IN_MILLISECONDS ? MIN_TIMEOUT_IN_MILLISECONDS : ms;
 
-    clearTimeout(timeoutIdRef.current);
-    timeoutIdRef.current = setTimeout(() => {
-      if (func) {
-        func();
-      }
-    }, timeout);
-  };
+        clearTimeout(timeoutIdRef.current);
+        timeoutIdRef.current = setTimeout(() => {
+            func();
+        }, timeout);
+    };
 };
 
 export default useDebounce;
